107d4f137995dff238a19cd6989e195d1e6a6f60
Anton Emelyanov <emelyanov.a.v.mail@gmail.com>
2025-03-31 18:18:39 +0500

    добавлены связи компонентов со стором

--- src/components/burger-ingredient/burger-ingredient.tsx
+++ src/components/burger-ingredient/burger-ingredient.tsx
@@ -1,14 +1,30 @@
 import { FC, memo } from 'react';
 import { useLocation } from 'react-router-dom';
+import { nanoid } from '@reduxjs/toolkit';
 
 import { BurgerIngredientUI } from '@ui';
 import { TBurgerIngredientProps } from './type';
+import { useDispatch } from '../../services/store';
+import {
+  setBurgerBun,
+  addTopping,
+  setSauce
+} from '../../slices/burger-constructor-slice';
 
 export const BurgerIngredient: FC<TBurgerIngredientProps> = memo(
   ({ ingredient, count }) => {
     const location = useLocation();
+    const dispatch = useDispatch();
 
-    const handleAdd = () => {};
+    const handleAdd = () => {
+      if (ingredient.type === 'bun') {
+        dispatch(setBurgerBun({ ingredient, id: nanoid() }));
+      } else if (ingredient.type === 'main') {
+        dispatch(addTopping({ ingredient, id: nanoid() }));
+      } else {
+        dispatch(setSauce({ ingredient, id: nanoid() }));
+      }
+    };
 
     return (
       <BurgerIngredientUI
--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,14 +1,25 @@
 import { FC } from 'react';
-import { Preloader } from '../ui/preloader';
-import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { Preloader } from '@ui';
+import { IngredientDetailsUI } from '@ui';
+import { useParams } from 'react-router-dom';
+import { useSelector } from '../../services/store';
+import { getAllComponents } from '../../slices/burger-constructor-slice';
 
-export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+export const IngredientDetails: FC<{ isPrimary?: boolean }> = ({
+  isPrimary = false
+}) => {
+  const { id } = useParams();
+  const components = useSelector(getAllComponents);
+  const ingredientData = components.find((item) => item._id == id);
 
   if (!ingredientData) {
     return <Preloader />;
   }
 
-  return <IngredientDetailsUI ingredientData={ingredientData} />;
+  return (
+    <IngredientDetailsUI
+      isPrimary={isPrimary}
+      ingredientData={ingredientData}
+    />
+  );
 };
